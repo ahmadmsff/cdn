c712319f9904ce43c95fccd7565b65f5775e7fd3
Achmad Musyaffa Taufiqi <37728550+ahmadmsff@users.noreply.github.com>
2019-06-13 05:57:38 +0700

    Update en-link.js

--- en-link.js
+++ en-link.js
@@ -1,5 +1,5 @@
 var itemsURL = Array("https://en-link.blogspot.com/2019/04/ini-manfaat-menyantap-bawang-putih.html?q=", "https://en-link.blogspot.com/2019/04/cara-memasak-cumi-asin-tanpa-bau-amis.html?q=");
-var itemURL = items[Math.floor(Math.random()*itemsURL.length)];
+var itemURL = itemsURL[Math.floor(Math.random()*itemsURL.length)];
 
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
 output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
